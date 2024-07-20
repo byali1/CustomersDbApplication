cbc4b6866d5b4b61251144c83332f14763445916
byali <ali_ilgin@outlook.com>
2024-07-20 19:12:00 +0300

    some fixes

--- Entities/Concrete/City.cs
+++ Entities/Concrete/City.cs
@@ -10,6 +10,7 @@ namespace Entities.Concrete
     public class City : IEntity
     {
         public int CityId { get; set; }
+        public int CountryId { get; set; }
         public string CityName { get; set; }
     }
 }
--- Entities/Concrete/District.cs
+++ Entities/Concrete/District.cs
@@ -10,6 +10,7 @@ namespace Entities.Concrete
     public class District : IEntity
     {
         public int DistrictId { get; set; }
+        public int CityId { get; set; }
         public string DistrictName { get; set; }
     }
 }
